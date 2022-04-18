3048ed0cad1573b8ade77e969a16d977fff18cad
Thiago Leao <thiagogorgulho@outlook.com>
2022-04-18 18:12:29 -0300

    Sum slices

---
+++ arithmetic/sum_all.go
@@ -0,0 +1,11 @@
+package arithmetic
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+	sums = make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
+}
